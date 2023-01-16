06d204b206da1f9d03d4f6dd5c8582bcb089525d
Srinarsimha <117781739+Srinarsimha@users.noreply.github.com>
2023-01-16 23:13:36 +0000

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Sri is an DevOps Engineer" );
+        System.out.println( "Hello World! Sri is an DevOps Engineer, End of this year I'll be become Software Engineer" );
     }
 }
